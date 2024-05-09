4492b217ab46cb022ca2b3fc88374b593e3b96eb
taharakroukii <2044661@cegeplimoilou.ca>
2024-05-09 12:30:39 -0400

    update front-end

--- tp4/src/Component/Login.js
+++ tp4/src/Component/Login.js
@@ -1,13 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
-    // Vérifier les informations d'identification dans la base de données
-    // Si les informations sont valides, rediriger vers la page Boutique
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('/login', { username, password });
+      console.log(response.data);
+      // Rediriger l'utilisateur vers la page de la boutique après un login réussi
+      window.location.href = '/boutique';
+    } catch (error) {
+      console.error(error);
+      // Afficher un message d'erreur à l'utilisateur
+    }
   };
+
+  useEffect(() => {
+    if (username !== '' && password !== '') {
+      handleLogin();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [username, password]);
 
   return (
     <div>
@@ -24,7 +39,7 @@ const Login = () => {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <button onClick={handleLogin}>Se connecter</button>
+      <button onClick={() => handleLogin()}>Se connecter</button>
     </div>
   );
 };
