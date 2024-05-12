7625028180f2288ab2b6c051aff8047a38aebc9d
taharakroukii <2044661@cegeplimoilou.ca>
2024-05-12 10:53:32 -0400

    Étape 4: Sécurisation complexité du mot de passe

--- tp4/src/Component/Enregistrement.js
+++ tp4/src/Component/Enregistrement.js
@@ -1,6 +1,6 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Registration = () => {
 
@@ -8,18 +8,21 @@ const Registration = () => {
   const [password, setPassword] = useState('');
   const nav = useNavigate();
 
-    // Effectuer une action après chaque changement de l'état d'enregistrement
-    // Par exemple, rediriger l'utilisateur vers la page de connexion après un enregistrement réussi
+  // Effectuer une action après chaque changement de l'état d'enregistrement
+  // Par exemple, rediriger l'utilisateur vers la page de connexion après un enregistrement réussi
 
   const handleRegistration = async () => {
-   if(new RegExp("\\w+\\s?").test(username) && new RegExp("\\w{3,}").test(password)) {
-    await axios.post('http://localhost:3006/enregistrer', { username, password })
-      .then((response) => {
-        console.log(response.data);
-        console.log("Enregistrement réussi ! Redirection vers la page de connexion...");
-        nav('/login');
-    }).catch(error => console.log(error));
-   }
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/;
+    const noConsecutiveChars = /(.)\1{2}/;
+
+    if (new RegExp("\\w+\\s?").test(username) && passwordRegex.test(password) && !noConsecutiveChars.test(password)) {
+      await axios.post('http://localhost:3006/enregistrer', { username, password })
+        .then((response) => {
+          console.log(response.data);
+          console.log("Enregistrement réussi ! Redirection vers la page de connexion...");
+          nav('/login');
+        }).catch(error => console.log(error));
+    }
   };
 
   return (
