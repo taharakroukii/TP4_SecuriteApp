b948a9763cdbbdabaad2c1caeb24765c3750679c
taharakroukii <2044661@cegeplimoilou.ca>
2024-05-15 10:57:58 -0400

    etape 5

--- tp4/src/Component/Login.js
+++ tp4/src/Component/Login.js
@@ -1,43 +1,3 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-
-const Login = (props) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-
-
-  const handleLogin = () => {
-    axios.post('http://localhost:3006/login', { username, password })
-      .then((response) => {
-        response.data.msg ? console.log(response.data.msg) : window.location.replace("http://localhost:3000/boutique");
-      }).catch(error => console.log(error));
-};
-
-   
-
-  return (
-    <div>
-      <h2>Login</h2>
-      <input
-        type="text"
-        placeholder="Nom d'utilisateur"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Mot de passe"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={() => handleLogin()}>Se connecter</button>
-    </div>
-  );
-};
-
-export default Login;
-
 // import React, { useState } from 'react';
 // import axios from 'axios';
 
@@ -46,13 +6,15 @@ export default Login;
 //   const [password, setPassword] = useState('');
 
 
-//   const handleLogin = () => {
-//       axios.post('http://localhost:3006/login', { username, password })
-//         .then((response) => {
-//           response.data.msg ? console.log(response.data.msg) : window.location.replace("http://localhost:3000/boutique");
-//         }).catch(error => console.log(error));
-//   };
 
+//   const handleLogin = () => {
+//     axios.post('http://localhost:3006/login', { username, password })
+//       .then((response) => {
+//         response.data.msg ? console.log(response.data.msg) : window.location.replace("http://localhost:3000/boutique");
+//       }).catch(error => console.log(error));
+// };
+
+   
 
 //   return (
 //     <div>
@@ -75,6 +37,77 @@ export default Login;
 // };
 
 // export default Login;
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Login = (props) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [ipBlacklist, setIpBlacklist] = useState([]);
+  const [accessDenied, setAccessDenied] = useState(false);
+
+  useEffect(() => {
+    // Charger la liste des adresses IP non fiables depuis le fichier ip.txt
+    axios.get('/ip.txt')
+      .then((response) => {
+        // Diviser les lignes du fichier en un tableau d'adresses IP
+        const ips = response.data.split('\n').map(ip => ip.trim());
+        setIpBlacklist(ips);
+      }).catch(error => console.log(error));
+  }, []);
+
+  const handleLogin = () => {
+    // Vérifier si l'adresse IP de l'utilisateur est dans la liste des adresses non fiables
+    const userIp = '172.20.32.20'; // Remplacez ceci par la vraie adresse IP de l'utilisateur
+
+    if (ipBlacklist.includes(userIp)) {
+      // Si l'adresse IP est dans la liste noire, afficher un message d'accès refusé
+      setAccessDenied(true);
+      console.log("Accès refusé. Votre adresse IP est bloquée.");
+      return; // Ne pas effectuer la requête de connexion
+    }
+
+    // Si l'adresse IP n'est pas dans la liste noire, effectuer la requête de connexion
+    axios.post('http://localhost:3006/login', { username, password })
+      .then((response) => {
+        response.data.msg ? console.log(response.data.msg) : window.location.replace("http://localhost:3000/boutique");
+      }).catch(error => console.log(error));
+  };
+
+  return (
+    <div>
+      {accessDenied ? (
+        <div>
+          <h2>Accès refusé</h2>
+          <p>Votre adresse IP est bloquée.</p>
+        </div>
+      ) : (
+        <div>
+          <h2>Login</h2>
+          <input
+            type="text"
+            placeholder="Nom d'utilisateur"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Mot de passe"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={() => handleLogin()}>Se connecter</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Login
+
+
+
 
 
 
