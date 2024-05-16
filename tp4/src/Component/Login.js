c1a9c37ea7fcf4745bc37a89390c534baddf4194
taharakroukii <2044661@cegeplimoilou.ca>
2024-05-16 07:42:34 -0400

    Organiser le code

--- tp4/src/Component/Login.js
+++ tp4/src/Component/Login.js
@@ -1,3 +1,6 @@
+/********************************* Etape 0  ***************************************************/
+
+
 // import React, { useState } from 'react';
 // import axios from 'axios';
 
@@ -38,78 +41,78 @@
 // export default Login;
 
 /********************************* IP  ***************************************************/
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
 
-const Login = (props) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [ipBlacklist, setIpBlacklist] = useState([]);
-  const [accessDenied, setAccessDenied] = useState(false);
-  const [error, setError] = useState('');
+// const Login = (props) => {
+//   const [username, setUsername] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [ipBlacklist, setIpBlacklist] = useState([]);
+//   const [accessDenied, setAccessDenied] = useState(false);
+//   const [error, setError] = useState('');
 
-  useEffect(() => {
-    // Charger la liste des adresses IP non fiables depuis le fichier ip.txt
-    axios.get('http://localhost:3006/ips') // Utilisation du chemin relatif
-      .then((response) => {
-        // Diviser les lignes du fichier en un tableau d'adresses IP
-        console.log(response.data)
-        const ips = response.data.split('\n').map(ip => ip.trim());
-        setIpBlacklist(ips);
-      }).catch(error => console.log(error));
-  }, []);
+//   useEffect(() => {
+//     // Charger la liste des adresses IP non fiables depuis le fichier ip.txt
+//     axios.get('http://localhost:3006/ips') // Utilisation du chemin relatif
+//       .then((response) => {
+//         // Diviser les lignes du fichier en un tableau d'adresses IP
+//         console.log(response.data)
+//         const ips = response.data.split('\n').map(ip => ip.trim());
+//         setIpBlacklist(ips);
+//       }).catch(error => console.log(error));
+//   }, []);
 
-  const handleLogin = () => {
-    // Votre logique de connexion ici
-    const userIp = '172.20.32.20'; 
+//   const handleLogin = () => {
+//     // Votre logique de connexion ici
+//     const userIp = '172.20.32.20'; 
 
-    if (ipBlacklist.includes(userIp)) {
-      // Si l'adresse IP est dans la liste noire, afficher un message d'accès refusé
-      setAccessDenied(true);
-      setError("Accès refusé. Votre adresse IP est bloquée.");
-      return; // Ne pas effectuer la requête de connexion
-    }
+//     if (ipBlacklist.includes(userIp)) {
+//       // Si l'adresse IP est dans la liste noire, afficher un message d'accès refusé
+//       setAccessDenied(true);
+//       setError("Accès refusé. Votre adresse IP est bloquée.");
+//       return; // Ne pas effectuer la requête de connexion
+//     }
 
-    // Si l'adresse IP n'est pas dans la liste noire, effectuer la requête de connexion
-    axios.post('http://localhost:3006/login', { username, password })
-      .then((response) => {
-        response.data.msg ? console.log(response.data.msg) : window.location.replace("/boutique");
-      }).catch(error => {
-        setError("Une erreur s'est produite lors de la connexion. Veuillez réessayer.");
-        console.log(error);
-      });
-  };
+//     // Si l'adresse IP n'est pas dans la liste noire, effectuer la requête de connexion
+//     axios.post('http://localhost:3006/login', { username, password })
+//       .then((response) => {
+//         response.data.msg ? console.log(response.data.msg) : window.location.replace("/boutique");
+//       }).catch(error => {
+//         setError("Une erreur s'est produite lors de la connexion. Veuillez réessayer.");
+//         console.log(error);
+//       });
+//   };
 
-  return (
-    <div>
-      {accessDenied ? (
-        <div>
-          <h2>Accès refusé</h2>
-          <p>{error}</p>
-        </div>
-      ) : (
-        <div>
-          <h2>Login</h2>
-          <input
-            type="text"
-            placeholder="Nom d'utilisateur"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Mot de passe"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button onClick={() => handleLogin()}>Se connecter</button>
-        </div>
-      )}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       {accessDenied ? (
+//         <div>
+//           <h2>Accès refusé</h2>
+//           <p>{error}</p>
+//         </div>
+//       ) : (
+//         <div>
+//           <h2>Login</h2>
+//           <input
+//             type="text"
+//             placeholder="Nom d'utilisateur"
+//             value={username}
+//             onChange={(e) => setUsername(e.target.value)}
+//           />
+//           <input
+//             type="password"
+//             placeholder="Mot de passe"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//           />
+//           <button onClick={() => handleLogin()}>Se connecter</button>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
 
-export default Login;
+// export default Login;
 
 /********************************* CAPTCHA ***************************************************/
 // import React, { useState, useEffect } from 'react';
@@ -185,52 +188,52 @@ export default Login;
 // export default Login;
 
 /********************************* 2FA ***************************************************/
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import ModalDeuxFacteur from './2FA/ModalDeuxFacteur';
+import React, { useState } from 'react';
+import axios from 'axios';
+import ModalDeuxFacteur from './2FA/ModalDeuxFacteur';
 
 
-// const Login = (props) => {
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [showTwoFactorModal,setShowTwoFactorModal] = useState(false);
+const Login = (props) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [showTwoFactorModal,setShowTwoFactorModal] = useState(false);
 
   
-//   const handleLogin = () => {
-//     axios.post('http://localhost:3006/login', { username, password })
-//       .then((response) => {
-//         if(response.data.msg ){
-//           console.log(response.data.msg) 
-//         }else{
-//           console.log(response.data);
-//           setShowTwoFactorModal(true);
-//         }
+  const handleLogin = () => {
+    axios.post('http://localhost:3006/login', { username, password })
+      .then((response) => {
+        if(response.data.msg ){
+          console.log(response.data.msg) 
+        }else{
+          console.log(response.data);
+          setShowTwoFactorModal(true);
+        }
 
-//       }).catch(error => console.log(error));
-//   };
+      }).catch(error => console.log(error));
+  };
 
-//   return (
-//     <div>
-//       <h2>Login</h2>
-//       <input
-//         type="text"
-//         placeholder="Nom d'utilisateur"
-//         value={username}
-//         onChange={(e) => setUsername(e.target.value)}
-//       />
-//       <input
-//         type="password"
-//         placeholder="Mot de passe"
-//         value={password}
-//         onChange={(e) => setPassword(e.target.value)}
-//       />
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        placeholder="Nom d'utilisateur"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Mot de passe"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
 
-//       <button onClick={() => handleLogin()}>Se connecter</button>
+      <button onClick={() => handleLogin()}>Se connecter</button>
 
-//       {showTwoFactorModal && <ModalDeuxFacteur/>}
-//     </div>
-//   );
-// };
+      {showTwoFactorModal && <ModalDeuxFacteur/>}
+    </div>
+  );
+};
 
 
-// export default Login;
+export default Login;
